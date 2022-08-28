a893ec28f61ab1a233a558facba7da5b75ca26d6
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-08-28 22:40:09 +0600

    modified:   src/redux/slices/post.js

--- src/redux/slices/post.js
+++ src/redux/slices/post.js
@@ -6,6 +6,8 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     return data;
 });
 
+
+
 const initialState = {
   // статья
   posts: {
@@ -20,9 +22,29 @@ const initialState = {
 };
 
 const postsSlice = createSlice({
-    name: "post",
-    initialState,
-    reducer: {},
+  name: "post",
+  initialState,
+  reducers: {},
+  //состояние нашего асинхронного акшена
+  extraReducers: {
+    //состояние загрузки
+    [fetchPosts.pending]: (state) => {
+      //обнуляем массив постов
+      state.posts.items = [];
+      //Если пришел запрос pending, то у части initialState/posts обновляем статус
+      state.posts.status = "loading";
+    },
+    //состояние когда запрос выполнился успешно
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = "loaded";
+    },
+    //состояние ошибки
+    [fetchPosts.rejected]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "error";
+    },
+  },
 });
 
 export const postsReducer = postsSlice.reducer; // экспортируем редюсер
